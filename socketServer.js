76e6edae3f212ee353b294175a464afd52d786da
jesushc <jesushc@unam.mx>
2017-06-22 19:38:49 -0500

    Chat preparado

--- socketServer.js
+++ socketServer.js
@@ -4,11 +4,16 @@ var socketServer = require('socket.io').listen(8082);
 
 
 socketServer.on('connect', function(socketClient) {
-  socketClient.join("cuarto1");
-  socketServer.to("cuarto1").emit("respServer","conectado al server");
+
+  socketClient.on('unirse',function(data){
+    console.log(data);
+    socketClient.join(data.cuarto);
+    data.mensaje="conectado..."
+    socketServer.to(data.cuarto).emit("unionExitosa",data);
+  });
 
   socketClient.on('mensaje',function(data){
-      console.log(data);
+    console.log(data);
       socketServer.to(data.cuarto).emit("chat",data);
   });
 
